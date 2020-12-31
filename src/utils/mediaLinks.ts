85ec5bbf52b308ee2564b396f7cd00beb06fea84
Branko <branko.blagojevic@gmail.com>
2020-12-31 11:04:17 -0500

    update paypal

--- src/utils/mediaLinks.ts
+++ src/utils/mediaLinks.ts
@@ -1,6 +1,6 @@
 export const MediaLinks = {
   twitter: "https://twitter.com/stevevarleyshow",
   instagram: "https://www.instagram.com/stevevarleyshow/",
-  paypal: "https://www.paypal.com/webapps/hermes?flow=1-P&ulReturn=true&token=10N98133FT862181S&mfid=1603362746254_211a916abb2ab&useraction=commit&country.x=US&locale.x=en_US#/checkout/review",
+  paypal: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=J3QEXBCCLYUC6",
   youtube: "https://www.youtube.com/c/SteveVarley",
 }
