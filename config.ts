9edd31af24fdc202c3144a3d5cd934a427074d41
Branko <branko.blagojevic@gmail.com>
2021-07-17 15:08:40 -0400

    remove youtube from description

--- config.ts
+++ config.ts
@@ -1,11 +1,11 @@
 export const MY_SEO = {
-  title: 'Steve Varley Show',
-  description: 'Steve Varley is a Youtube film/tv critic and celebrity interviewer.',
+  title: 'Steve Varley',
+  description: 'Steve Varley is a film/tv critic and celebrity interviewer.',
   openGraph: {
       type: 'website',
       url: 'https://www.stevevarley.com',
       title: 'Steve Varley',
-      description: 'Steve Varley is a Youtube film/tv critic and celebrity interviewer.',
+      description: 'Steve Varley is a film/tv critic and celebrity interviewer.',
       image: '/steve-varley.jpg',
   }
 };
