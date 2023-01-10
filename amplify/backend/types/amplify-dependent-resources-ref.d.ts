490b4d13f02c152a382840128135b69d8029b9ad
Branko <branko.blagojevic@gmail.com>
2023-01-09 21:42:17 -0500

    api key

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,28 @@
 export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "stevevarley": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "auth": {
-        "stevevarley7d97c051": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "AppClientSecret": "string"
-        },
-        "userPoolGroups": {
-            "adminGroupRole": "string"
-        }
-    },
-    "storage": {
-        "images": {
-            "BucketName": "string",
-            "Region": "string"
-        }
+  "api": {
+    "stevevarley": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
     }
+  },
+  "auth": {
+    "stevevarley7d97c051": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "adminGroupRole": "string"
+    }
+  },
+  "storage": {
+    "images": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
 }
